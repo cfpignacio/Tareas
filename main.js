a085710c488cdd9b25a2ebba4349cdd8640f1e9b
Ignacio Camillucci <ignacio.camillucci@gmail.com>
2023-04-24 19:49:30 -0300

    fix preventdefault

--- main.js
+++ main.js
@@ -1,21 +1,48 @@
 const formulario = document.querySelector('form');
 const tareaInput = document.querySelector('#tarea');
 const guardarBtn = document.querySelector('#guardar');
+const borrarBtn = document.querySelector('#borrar');
+const listaTareas = document.querySelector('#listaTareas');
 
-guardarBtn.addEventListener('click', () => {
+guardarBtn.addEventListener('click', (event) => {
+	event.preventDefault();
 	const tarea = tareaInput.value;
 	let tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+
+	if (tarea === '') {
+		return alert('La tarea no puede estar vacia');
+	}
+
 	tareas.push(tarea);
 	localStorage.setItem('tareas', JSON.stringify(tareas));
-	alert('Tarea guardada en LocalStorage');
+	cargarTareas();
+	formulario.reset();
+	console.log('Se agrego correctamente la tarea ingresada');
 });
 
 window.addEventListener('load', () => {
 	let tareas = JSON.parse(localStorage.getItem('tareas')) || [];
-	const listaTareas = document.querySelector('#listaTareas');
 	tareas.forEach((tarea) => {
 		const li = document.createElement('li');
 		li.textContent = tarea;
 		listaTareas.appendChild(li);
 	});
+});
+
+const cargarTareas = () => {
+	listaTareas.innerHTML = '';
+	let tareas = JSON.parse(localStorage.getItem('tareas')) || [];
+	tareas.forEach((tarea) => {
+		const li = document.createElement('li');
+		li.textContent = tarea;
+		listaTareas.appendChild(li);
+	});
+};
+
+// capturar el evento click del boton guardar
+// borrar todas las tareas que se encuentran el localstorage
+// con la key tareas
+
+borrarBtn.addEventListener('click', () => {
+	localStorage.removeItem('tareas');
 });
